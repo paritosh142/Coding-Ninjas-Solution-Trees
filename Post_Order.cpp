647cdc640d3eb407a28a548a6b56ffc9e469d095
paritosh142 <79096636+paritosh142@users.noreply.github.com>
2022-08-21 17:17:28 +0530

    Update Post_Order.cpp

--- Post_Order.cpp
+++ Post_Order.cpp
@@ -1,25 +1,38 @@
+// void printPostOrder(TreeNode<int>* root) {
+//     // Write your code here
+    
+//     if(root  ==  NULL)
+//     {
+//         return;
+//     }
+    
+// //     if(root -> children.size() == 0)
+// //     {
+// // //         
+// //     }
+    
+    
+//     for(int i = 0 ; i < root -> children.size() ; i++)
+//     {
+        
+//         if(root -> children.size() == 0)
+//         {
+//             cout<<root -> children[i] -> data <<" ";
+//         }
+        
+//         printPostOrder(root -> children[i]);
+//     }
+// }
+
 void printPostOrder(TreeNode<int>* root) {
     // Write your code here
     
-    if(root  ==  NULL)
+    
+    for(int i=0;i<root->children.size();i++)
     {
-        return;
-    }
-    
-//     if(root -> children.size() == 0)
-//     {
-// //         
-//     }
-    
-    
-    for(int i = 0 ; i < root -> children.size() ; i++)
-    {
+        printPostOrder(root->children[i]);
         
-        if(root -> children.size() == 0)
-        {
-            cout<<root -> children[i] -> data <<" ";
-        }
-        
-        printPostOrder(root -> children[i]);
     }
+    cout<<root->data<<" ";
+
 }
