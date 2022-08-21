20b7bc7c01b47d625e5b85b67548e1e9ed645653
paritosh142 <79096636+paritosh142@users.noreply.github.com>
2022-08-21 17:15:56 +0530

    Create Post_Order.cpp

---
+++ Post_Order.cpp
@@ -0,0 +1,25 @@
+void printPostOrder(TreeNode<int>* root) {
+    // Write your code here
+    
+    if(root  ==  NULL)
+    {
+        return;
+    }
+    
+//     if(root -> children.size() == 0)
+//     {
+// //         
+//     }
+    
+    
+    for(int i = 0 ; i < root -> children.size() ; i++)
+    {
+        
+        if(root -> children.size() == 0)
+        {
+            cout<<root -> children[i] -> data <<" ";
+        }
+        
+        printPostOrder(root -> children[i]);
+    }
+}
